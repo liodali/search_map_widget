528a1cbb6fc890c9a59caade17614834f6bc0ecd
dali <lionelviperdali@gmail.com>
2021-11-21 16:08:23 +0100

    feat : create notification for advanced search map * create advanced_search_notification to get currentChildSize

---
+++ lib/src/notification/advanced_search_notification.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/widgets.dart';
+
+class AdvancedSearchNotification extends Notification with ViewportNotificationMixin {
+  /// Initializes fields for subclasses.
+  AdvancedSearchNotification({
+    required this.metrics,
+    required this.context,
+  });
+
+  /// The fraction to scale the child's position value.
+  /// between maxChildSize and minChildSize
+  /// this value is normalized between 0 and 1
+  /// where 0 is minChildSize and 1 is maxChildSize
+  final double metrics;
+
+  /// The build context of the widget that fired this notification.
+  final BuildContext? context;
+
+  @override
+  void debugFillDescription(List<String> description) {
+    super.debugFillDescription(description);
+    description.add('$metrics');
+  }
+}
